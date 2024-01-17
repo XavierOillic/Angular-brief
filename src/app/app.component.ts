9c944b7fa97384cfa83640a2d231696763e0fad9
Xavier OILLIC <xavieroillic@MacBook-Pro-de-Xavier-OILLIC.local>
2024-01-17 11:35:35 +0100

    first one

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    console.log(this.dataService.getUsersData());
+  }
+}
---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.css'],
+})
+export class DataTableComponent implements OnInit {
+  dataUser = [];
+  constructor() {}
+
+  ngOnInit() {}
+}
